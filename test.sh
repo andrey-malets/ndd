668ffe4461f563208e9606500f0b6c336ac82091
Дмитрий Зубарев <mathcrosp@yandex.ru>
2016-05-29 01:23:18 +0500

    Removed unnecessary code, added named args in test.sh.

--- test.sh
+++ test.sh
@@ -5,58 +5,57 @@ NDD_DIR="$HOME/ndd/"
 TEST_DIR=$NDD_DIR"test/"
 PORT=3687
 
-available=()
-unreachable=()
-
-check_hosts() {
-  for host in $@; do
-    ping -c 1 $host > /dev/null
-    if [ $? -eq 0 ]; then
-      available+=($host)
-    else
-      unreachable+=($host)
-    fi
-  done
-  if [ ${#unreachable[@]} -ne 0 ]; then
-    echo Unreachable hosts: ${unreachable[@]}
-  fi
-}
+hosts=()
 
 prepare_hosts() {
-  for host in ${available[@]}; do
-    if ! ssh $host [ -d $NDD_DIR ]; then
-      ssh $host mkdir $NDD_DIR
+  for host in ${hosts[@]}; do
+    if ! ssh "$host" [ -d "$NDD_DIR" ]; then
+      ssh "$host" mkdir "$NDD_DIR"
     fi
-    if ! ssh $host [ -d $TEST_DIR ]; then
-      ssh $host mkdir $TEST_DIR
+    if ! ssh "$host" [ -d "$TEST_DIR" ]; then
+      ssh "$host" mkdir "$TEST_DIR"
     fi
   done
 }
 
 send_driver() {
-  for host in ${available[@]}; do
-    scp $DRIVER $host:$NDD_DIR$DRIVER
+  for host in ${hosts[@]}; do
+    scp "$DRIVER" "$host:$NDD_DIR$DRIVER"
   done
 }
 
 get_slaves() {
   slaves=()
-  hosts=$@
   for host in ${hosts[@]}; do
-    slaves+=("-d "$host)
+    slaves+=("-d $host")
   done
   echo ${slaves[@]}
 }
 
 run() {
-  ip=$(/sbin/ifconfig | grep 172 | awk '{print $2}' | cut -d ':' -f2)
-  slaves=$(get_slaves $available)
-  python ndd_slurm.py -s $ip -i $TEST_DIR -o $TEST_DIR ${slaves[@]}\
-                      -p $PORT -H -r
+  slaves=$(get_slaves)
+  python ndd_slurm.py -s "$MASTER_IP" -i "$TEST_DIR" -o "$TEST_DIR"\
+                      ${slaves[@]} -p "$PORT" -H -r
 }
 
 main() {
-  check_hosts $@
+  while [[ $# > 1 ]]; do
+    key=$1
+    case $key in
+      -i)
+        MASTER_IP="$2"
+        shift
+        ;;
+      -d)
+        hosts+=($2)
+        shift
+        ;;
+      *)
+        usage "$0"
+        ;;
+    esac
+    shift
+  done
   prepare_hosts
   send_driver
   run
